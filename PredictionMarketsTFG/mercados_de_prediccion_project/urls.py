acac805762d3561f9f447e5463cf511e1372ea82
Antonio Rodríguez Artacho <antonio.rdgz.a@gmail.com>
2019-04-22 14:25:02 +0200

    Fixed URLs for authentication

--- PredictionMarketsTFG/mercados_de_prediccion_project/urls.py
+++ PredictionMarketsTFG/mercados_de_prediccion_project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+	path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('',include('mercados_de_prediccion.urls')),
 	url(r'^i18n/', include('django.conf.urls.i18n')),
 ]
