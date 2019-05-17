9feed9d2855830e6f619e868827019913da29731
Antonio Rodríguez <antrodart@alum.us.es>
2019-05-17 17:10:50 +0200

    Edited parameters

--- PredictionMarketsTFG/mercados_de_prediccion_project/settings.py
+++ PredictionMarketsTFG/mercados_de_prediccion_project/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(__file__)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#PROJECT_DIR = os.path.dirname(__file__)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
