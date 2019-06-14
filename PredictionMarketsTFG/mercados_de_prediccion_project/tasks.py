ba4758ddc3240916e01b1cd10f42b9204b54e8dd
Antonio Rodríguez Artacho <antonio.rdgz.a@gmail.com>
2019-06-14 14:37:44 +0200

    Changed tasks.py method

--- PredictionMarketsTFG/mercados_de_prediccion_project/tasks.py
+++ PredictionMarketsTFG/mercados_de_prediccion_project/tasks.py
@@ -27,47 +27,47 @@ def register_prices_per_day():
 	today = datetime.datetime.now()
 	markets_to_register_prices = Market.objects.filter(end_date__gte=today)  #Markets that haven't finished or finish today
 
-	for market in markets_to_register_prices:
-		print("Entring the for loop for market: " + str(market))
-		if market.is_binary:  #  Binary markets
-			print("Market is binary")
-			yes_option = market.option_set.get(binary_yes=True)
-			no_option = market.option_set.get(binary_yes=False)
+	with transaction.atomic():
+		for market in markets_to_register_prices:
+			print("Entring the for loop for market: " + str(market))
+			if market.is_binary:  #  Binary markets
+				print("Market is binary")
+				yes_option = market.option_set.get(binary_yes=True)
+				no_option = market.option_set.get(binary_yes=False)
 
-			current_price_yes = yes_option.price_set.get(is_last=True)
-			current_price_no = no_option.price_set.get(is_last=True)
+				current_price_yes = yes_option.price_set.get(is_last=True)
+				current_price_no = no_option.price_set.get(is_last=True)
 
-			print("Yes option is: " + yes_option.name + ", and no option is: " + no_option.name + ". Entring the atomic method...")
-			overwrite_prices(current_price_yes, current_price_no)
-
-		else:  #Non binary markets
-			print("Market is not binary")
-			for option in market.option_set.all():
-				current_price_yes = option.price_set.get(is_last=True, is_yes=True)
-				current_price_no = option.price_set.get(is_last=True, is_yes=False)
-
-				print("Entring the atomic method for option " + option.name)
+				print("Yes option is: " + yes_option.name + ", and no option is: " + no_option.name + ". Entring the atomic method...")
 				overwrite_prices(current_price_yes, current_price_no)
+
+			else:  #Non binary markets
+				print("Market is not binary")
+				for option in market.option_set.all():
+					current_price_yes = option.price_set.get(is_last=True, is_yes=True)
+					current_price_no = option.price_set.get(is_last=True, is_yes=False)
+
+					print("Entring the atomic method for option " + option.name)
+					overwrite_prices(current_price_yes, current_price_no)
 
 	print("Register prices method finished.")
 
 def overwrite_prices(current_price_yes, current_price_no):
-	with transaction.atomic():
-		print("In the transaction. Setting old prices is_last to False, and saving them...")
-		#  Now the previous "last prices" are no longer the last prices.
-		current_price_yes.is_last = False
-		current_price_no.is_last = False
-		current_price_yes.save()
-		current_price_no.save()
+	print("In the overwrite_prices method. Setting old prices is_last to False, and saving them...")
+	#  Now the previous "last prices" are no longer the last prices.
+	current_price_yes.is_last = False
+	current_price_no.is_last = False
+	current_price_yes.save()
+	current_price_no.save()
 
-		print("Current price yes: " + str(current_price_yes) + " and current price no: " + str(current_price_no))
-		print("Going to create the new prices, copying the old ones and setting is_last to True.")
-		#  Creation of 2 new prices, one for each option, that are the last prices.
-		new_yes_price = Price.objects.create(option=current_price_yes.option, is_yes=True, is_last=True, buy_price=current_price_yes.buy_price)
-		new_no_price = Price.objects.create(option=current_price_no.option, is_yes=False, is_last=True, buy_price=current_price_no.buy_price)
+	print("Current price yes: " + str(current_price_yes) + " and current price no: " + str(current_price_no))
+	print("Going to create the new prices, copying the old ones and setting is_last to True.")
+	#  Creation of 2 new prices, one for each option, that are the last prices.
+	new_yes_price = Price.objects.create(option=current_price_yes.option, is_yes=True, is_last=True, buy_price=current_price_yes.buy_price)
+	new_no_price = Price.objects.create(option=current_price_no.option, is_yes=False, is_last=True, buy_price=current_price_no.buy_price)
 
-		print("Prices created. Yes price: " + new_yes_price + ". No price: " + str(new_no_price))
-		print("Finishing transaction method...")
+	print("Prices created. Yes price: " + str(new_yes_price) + ". No price: " + str(new_no_price))
+	print("Finishing overwrite_prices method...")
 
 
 print("Scheduling tasks...")
